cc598e633490da1874bde12e2c445a52dc8d7644
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2024-06-18 11:22:23 -0700

    Move datamining.zip into data folder
    Also remove the exclusion of .gitmodules from the .gitignore

--- onClone.sh
+++ onClone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # unzip jabref-5.0-alpha.zip
-unzip datamining.zip
+unzip data/datamining.zip
 mkdir -p output
 mv datamining.pkl output/datamining.pkl
 # git submodule update --init --recursive
